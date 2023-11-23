e957bd385bfa3739e15ca5ebbdbbf0bbea9903c7
Md Sarfraj <116196482+mdsarfarj@users.noreply.github.com>
2023-11-23 23:38:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 function firstChar(text) {
   // your code here
-	let ans=text.charAt(0);
-	return ans;
+	if (text === null || text === '') {
+        return '';
+    }
+
+    // Iterate through the characters in the string
+    for (let i = 0; i < text.length; i++) {
+        // Return the first non-space character
+        if (text[i] !== ' ') {
+            return text[i];
+        }
+    }
+
+    // If the string contains only spaces, return an empty string
+    return '';
 	
 }
 
